fab2edf07d78cf0f5739b8cbf9eb6972b71205a6
juan david <juandavid1307137@gmail.com>
2024-04-23 15:31:23 -0500

    version volver despues

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,34 +1,76 @@
-let venta = prompt("Bienvenido a nuestra web, aqui podras comprar nuestros productos, coloque el numero correspondiente para seleccionar los diferentes tipos de postres \n1) tortas \n2) cupcakes \n3) variedad")
-
 let compraT = 0;
+
+function idValidoT(id) {
+    let indice = -1
+
+    for (let i = 0; i < tortas.length; i++) {
+        if (tortas[i].id === id) {
+            indice = [i]
+        }
+    }
+    console.log(indice)
+    return indice
+}
+
+function comprarT() {
+    let idDelProducto = parseInt(prompt("ingresa el numero del producto que quiere comprar"))
+    console.log(idDelProducto)
+}
+
+class torta {
+    constructor(nombre, precio, id) {
+        this.nombre = nombre
+        this.precio = precio
+        this.id = id
+    }
+}
+
+const torta1 = new torta( "Torta de vainilla" , 10000 , 1)
+const torta2 = new torta( "Torta de chocolate" , 20000 , 2)
+const torta3 = new torta( "Torta de las tres leches" , 50000 , 3)
+const torta4 = new torta( "Torta de red velvet" , 100000 , 4)
+
+let tortas = [torta1, torta2, torta3, torta4]
+
+class cupcake {
+    constructor(nombre, precio, id) {
+        this.nombre = nombre
+        this.precio = precio
+        this.id = id
+    }
+}
+
+const cupcake1 = new cupcake( "Cupcake normal" , 5000 , 1)
+const cupcake2 = new cupcake( "Cupcake personalizado" , 7000 , 2)
+const cupcake3 = new cupcake( "Cupcake oreo" , 7000 , 3)
+const cupcake4 = new cupcake( "Cupcake oro" , 100000000000 , 4)
+
+let cupcakes = [cupcake1, cupcake2, cupcake3, cupcake4]
+
+class postre {
+    constructor(nombre, precio, id) {
+        this.nombre = nombre
+        this.precio = precio
+        this.id = id
+    }
+}
+
+const postre1 = new postre( "Postre limon" , 10000 , 9)
+const postre2 = new postre( "Postre maracuya" , 10000 , 10)
+const postre3 = new postre( "Gelatinas" , 5000 , 11)
+const postre4 = new postre( "Cheesecake" , 7000, 12)
+
+let postres = [postre1, postre2, postre3, postre4]
+
+let venta = prompt("Bienvenido a nuestra web, aqui podras comprar nuestros productos, coloque el numero correspondiente para seleccionar los diferentes tipos de postres \n1) tortas \n2) cupcakes \n3) variedad")
 
 while (venta == "1" || venta == "2"|| venta == "3") {
     switch (venta) {
         case "1":
-            let tortas = prompt("Aqui nuestras tortas, coloque el numero correspondiente para comprar \n1) Torta de vainilla, precio: $10.000 cop \n2) Torta de chocolate, precio: $20.000 cop \n3) Torta de las tres leches, precio: $50.000 cop \n4) Torta de red velvet, precio: $100.000 cop \n5) Cancelar compra\nEl precio total hasta ahora es de $" + compraT + " cop" +"\nPresiona cualquier letra o numero no disponible salir")
+            let tortasV = prompt("Aqui nuestras tortas, coloque el numero correspondiente para comprar \n1) Torta de vainilla, precio: $10.000 cop \n2) Torta de chocolate, precio: $20.000 cop \n3) Torta de las tres leches, precio: $50.000 cop \n4) Torta de red velvet, precio: $100.000 cop \n5) Cancelar compra\nEl precio total hasta ahora es de $" + compraT + " cop" +"\nPresiona cualquier letra o numero no disponible salir")
             while (tortas =="1" || tortas == "2" || tortas == "3" || tortas == "4" || tortas == "5") {
-                switch (tortas) {
-                    case "1":
-                        compraT = compraT + 10000
-                    break;
-                    case "2":
-                        compraT = compraT + 20000
-                    break;
-                    case "3":
-                        compraT = compraT + 50000
-                    break;
-                    case "4":
-                        compraT = compraT + 100000
-                    break;
-                    case "5" :
-                        compraT = 0
-                    break;
-                    default:
-                        prompt("gracias por comprar")
-                    break;
-                }
-                tortas = prompt("Aqui nuestras tortas, coloque el numero correspondiente para comprar \n1) Torta de vainilla, precio: $10.000 cop \n2) Torta de chocolate, precio: $20.000 cop \n3) Torta de las tres leches, precio: $50.000 cop \n4) Torta de red velvet, precio: $100.000 cop \n5) Cancelar compra\nEl precio total hasta ahora es de $" + compraT + " cop" +"\nPresiona cualquier letra o numero no disponible para comprar")
-            }
+                
+    }
             break;
         case "2":
             let cupcakes = prompt("Aqui nuestros cupcakes, coloque el numero correspondiente para comprar \n1) cupcake normal, precio: $5.000 cop \n2) cupcake con la decoracion que quieras, precio: $7.000 cop \n3) cupcake de oreo, precio: $7.000 cop \n4) cupcake de oro, precio: $100.000.000.000 cop \n5) Cancelar compra\nEl precio total hasta ahora es de $" + compraT + " cop" +"\nPresiona cualquier letra o numero no disponible salir")
